c0e41bf7cc714b8d256a1ab11e3ae43c8b437eb1
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-24 14:44:32 +0200

    added screenUtil

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,6 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      child: MaterialApp(),
+    );
   }
 }

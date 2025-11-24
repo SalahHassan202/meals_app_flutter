db1347c637ea66fa5ca407325602f1abc5f8c07a
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-24 14:45:55 +0200

    update app text style file

--- lib/core/styles/app_text_style.dart
+++ lib/core/styles/app_text_style.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:meals_app/core/styles/app_colors.dart';
 
 class AppTextStyle {
   static TextStyle onBoardingPrimaryStyle = GoogleFonts.inter(
-    fontSize: 32,
+    fontSize: 32.sp,
     fontWeight: FontWeight.w800,
     color: AppColors.whiteColor,
   );
   static TextStyle onBoardingSubTitleStyle = GoogleFonts.inter(
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.normal,
     color: AppColors.whiteColor,
   );
   static TextStyle grey14Regular = GoogleFonts.inter(
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.normal,
     color: const Color(0xff878787),
   );
   static TextStyle black16Medium = GoogleFonts.inter(
-    fontSize: 16,
+    fontSize: 16.sp,
     fontWeight: FontWeight.normal,
     color: AppColors.blackColor,
   );
   static TextStyle white14SemiBold = GoogleFonts.inter(
-    fontSize: 14,
+    fontSize: 14.sp,
     fontWeight: FontWeight.w500,
     color: AppColors.whiteColor,
   );

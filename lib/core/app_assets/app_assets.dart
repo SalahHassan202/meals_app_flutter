217a4f913af23ba66167f7097b5b2c974555edc1
SalahHassan202 <salahhassan66190@gmail.com>
2025-11-24 14:03:08 +0200

    added app assets file

---
+++ lib/core/app_assets/app_assets.dart
@@ -0,0 +1,5 @@
+class AppAssets {
+  static const String onBordingPics = "assets/images/on_boarding_pic.png";
+  static const String topPicHomeScreen = "assets/images/top_home_pics.png";
+  static const String foodExample = "assets/images/food_example.png";
+}

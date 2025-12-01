8a2012cc88ebea77fc610130906def0546651567
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-02 00:07:50 +0200

    update routing

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,6 +1,9 @@
 import 'package:go_router/go_router.dart';
 import 'package:meals_app/core/routing/app_routes.dart';
+import 'package:meals_app/features/add_meal_screen/add_meal_screen.dart';
+import 'package:meals_app/features/home_screen/data/models/meal_model.dart';
 import 'package:meals_app/features/home_screen/home_screen.dart';
+import 'package:meals_app/features/meal_details_screen/meal_details_screen.dart';
 import 'package:meals_app/features/on_boardong_Screen/on_boarding_screen.dart';
 
 class RouterGenerationConfig {
@@ -17,6 +20,19 @@ class RouterGenerationConfig {
         name: AppRoutes.homeScreen,
         path: AppRoutes.homeScreen,
         builder: (context, state) => const HomeScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.addMeal,
+        name: AppRoutes.addMeal,
+        builder: (context, state) => const AddMealScreen(),
+      ),
+      GoRoute(
+        path: AppRoutes.mealDetailsScreen,
+        name: AppRoutes.mealDetailsScreen,
+        builder: (context, state) {
+          Meal meal = state.extra as Meal;
+          return MealDetailsScreen(meal: meal);
+        },
       ),
     ],
   );

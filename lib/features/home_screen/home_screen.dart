1736567f013cfc1b8c96efc44c10f8ef08c90ab0
SalahHassan202 <salahhassan66190@gmail.com>
2025-12-01 22:38:06 +0200

    modify home screen

--- lib/features/home_screen/home_screen.dart
+++ lib/features/home_screen/home_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:meals_app/core/styles/app_text_style.dart';
 import 'package:meals_app/core/widgets/spacing_widget.dart';
+import 'package:meals_app/features/home_screen/widgets/custom_food_item_widget.dart';
 import 'package:meals_app/features/home_screen/widgets/custom_top_home_part.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -32,6 +33,30 @@ class _HomeScreenState extends State<HomeScreen> {
                   ),
                 ),
                 HightSpace(24),
+                FoodItemWidget(
+                  imageUrl:
+                      'https://pngtree.com/freepng/delicious-and-testy-cheese-burger_16763714.html',
+                  name: 'burger',
+                  rate: 4.5,
+                  time: '20/40',
+                  onTap: () {},
+                ),
+                FoodItemWidget(
+                  imageUrl:
+                      'https://pngtree.com/freepng/delicious-and-testy-cheese-burger_16763714.html',
+                  name: 'burger',
+                  rate: 4.5,
+                  time: '20/40',
+                  onTap: () {},
+                ),
+                FoodItemWidget(
+                  imageUrl:
+                      'https://pngtree.com/freepng/delicious-and-testy-cheese-burger_16763714.html',
+                  name: 'burger',
+                  rate: 4.5,
+                  time: '20/40',
+                  onTap: () {},
+                ),
               ],
             ),
           ],
